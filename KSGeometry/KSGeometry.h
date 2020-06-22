638634d11239853ce5bd57857771314c51607aea
Keith Staines <keith27staines@icloud.com>
2020-06-22 07:20:41 +0100

    Initial Commit

---
+++ KSGeometry/KSGeometry.h
@@ -0,0 +1,19 @@
+//
+//  KSGeometry.h
+//  KSGeometry
+//
+//  Created by Keith Staines on 22/06/2020.
+//  Copyright © 2020 Keith Staines. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KSGeometry.
+FOUNDATION_EXPORT double KSGeometryVersionNumber;
+
+//! Project version string for KSGeometry.
+FOUNDATION_EXPORT const unsigned char KSGeometryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KSGeometry/PublicHeader.h>
+
+
