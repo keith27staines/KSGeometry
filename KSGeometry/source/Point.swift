d9fc815250ea0c5b748d8ebdec78fc3460a944b3
Keith Staines <keith27staines@icloud.com>
2020-06-27 09:17:28 +0100

    Factored out XYLocatble
    into its own file

--- KSGeometry/source/Point.swift
+++ KSGeometry/source/Point.swift
@@ -1,20 +1,4 @@
 
-public protocol XYLocatable {
-    var x: Float { get }
-    var y: Float { get }
-    func isCoincidentWith(_ other: XYLocatable) -> Bool
-    func distance2From(_ other: XYLocatable) -> Float
-}
-
-public extension XYLocatable {
-    func isCoincidentWith(_ other: XYLocatable) -> Bool {
-        x == other.x && y == other.y
-    }
-    func distance2From(_ other: XYLocatable) -> Float {
-        (x - other.x)*(x - other.x) + (y - other.y)*(y - other.y)
-    }
-}
-
 public struct Point: XYLocatable, Hashable, Equatable {
     public static let zero = Point(x: 0, y: 0)
     public var x: Float
